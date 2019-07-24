79547166899b848e86238de54034e1141b6135f7
it01 <>
2019-07-24 13:45:06 +0800

    add project1

---
+++ SwiftProject/Swift1/Swift1ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Swift1ViewController.swift
+//  SwiftProject
+//
+//  Created by it01 on 2019/7/24.
+//  Copyright © 2019 bear. All rights reserved.
+//
+
+import UIKit
+
+class Swift1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
